c2fbc4561188da4782e3834ee839333708370c5a
htx7 <bc7silva@gmail.com>
2024-04-10 11:00:28 -0300

    sorting algs evaluation

--- Homework/HW1/src/main.cc
+++ Homework/HW1/src/main.cc
@@ -139,14 +139,19 @@ void swap(int *xp, int *yp, sortperf_t *s){
 
 // shellsort
 void shellSort(int *A, int n, sortperf_t * s) {
+  inccalls(s,1);
   for(int h = n / 2; h > 0; h /= 2){
     for(int i = h; i < n; i += 1){
       int temp = A[i];
+      incmove(s,1);
       int j;
       for(j = i; j >= h && A[j - h] > temp; j -= h){
+        inccmp(s,2);
         A[j] = A[j - h];
+        incmove(s,1);
       }
       A[j] = temp;
+      incmove(s,1);
     }
   }
 }
@@ -199,13 +204,16 @@ void insertionSort(int v[], int l, int r, sortperf_t * s) {
     inccalls(s,1);
     for (i = 1; i <= r; i++){
         aux = v[i];
+        incmove(s,1);
         j = i - 1;
         while ((j >= 0) && aux < v[j]){
             inccmp(s,2);
             v[j + 1] =  v[j];
+            incmove(s,1);
             j--;
         }
         v[j + 1] = aux;
+        incmove(s,1);
     }
 }
 
